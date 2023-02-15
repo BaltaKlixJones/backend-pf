ff7ca421cdd6cfdf1e6424729bafa25f455037cc
BaltaKlixJones <balta98_@hotmail.com>
2023-02-15 11:26:13 -0300

    intento de deploy

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-
+//aaa
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME , DB_DEPLOY} = process.env;
 
 // const sequelize = new Sequelize(
